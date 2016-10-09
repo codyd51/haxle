9d8f19d62468aaf12ac021db7c864c69280fa352
Phillip Tennen <phillip.ennen@gmail.com>
2016-10-09 17:03:09 -0600

    Simplistic virtual machine (asm interpreter)

---
+++ vm.c
@@ -0,0 +1,85 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+#include <stdbool.h>
+#include <string.h>
+
+#define NUM_REGS 4
+typedef struct cpu_state {
+	unsigned regs[NUM_REGS];
+} cpu_state;
+
+typedef struct instruction {
+	int instruction;
+	int reg_op0, reg_op1, reg_op2;
+	int immediate;
+} instruction;
+
+int fetch(int* prog) {
+	//start at PC = 0;
+	static unsigned pc = 0;
+	return prog[pc++];
+}
+
+#define HLT_INSTR  0x00
+#define LOAD_INSTR 0x01
+#define ADD_INSTR  0x02
+
+instruction* decode(unsigned instr) {
+	instruction* ret = malloc(sizeof(ret));
+	ret->instruction = (instr & 0xF000) >> 12;
+	ret->reg_op0	 = (instr & 0xF00 ) >> 8;
+	ret->reg_op1	 = (instr & 0xF0  ) >> 4;
+	ret->reg_op2	 = (instr & 0xF   );
+	ret->immediate	 = (instr & 0xFF  );
+	return ret;
+}
+
+static bool running = true;
+void eval(cpu_state* state, instruction* instr) {
+	switch (instr->instruction) {
+		case LOAD_INSTR:
+			printf("LOAD R%d, #%d", instr->reg_op0, instr->immediate);
+			state->regs[instr->reg_op0] = instr->immediate;
+			break;
+		case ADD_INSTR:
+			printf("ADD R%d, R%d + R%d", instr->reg_op0, instr->reg_op1, instr->reg_op2);
+			state->regs[instr->reg_op0] = instr->reg_op1 + instr->reg_op2;
+			break;
+		default:
+			printf("Unknown instruction %x!\n", instr->instruction);
+			//fall through to HLT case
+		case HLT_INSTR:
+			printf("HLT");
+			running = false;
+			break;
+	}
+	printf("\n");
+}
+
+void dump_cpu_state(cpu_state* state) {
+	for (int i = 0; i < NUM_REGS; i++) {
+		printf("R%d = %d\t", i, state->regs[i]);
+	}
+	printf("\n");
+}
+
+void run(int* prog) {
+	cpu_state* state = malloc(sizeof(cpu_state));
+	memset(state, 0, sizeof(cpu_state));
+
+	while (running) {
+		unsigned instr = fetch(prog);
+		instruction* real = decode(instr);
+		eval(state, real);
+		dump_cpu_state(state);
+	}
+}
+
+int main(int argc, char** argv) {
+	int prog[] = {0x1064, 0x11C8, 0x2201, 0x0000};
+	run(prog);
+
+	return 0;
+}
+
