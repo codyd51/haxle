fa54d39b34e2f5415a515c4b1acfb293c524b0e4
Phillip Tennen <phillip.ennen@gmail.com>
2016-10-10 14:34:11 -0600

    Refactor entire VM state into instance struct

--- vm.c
+++ vm.c
@@ -5,10 +5,17 @@ typedef struct cpu_state {
 	unsigned regs[NUM_REGS];
 } cpu_state;
 
-int fetch(int* prog) {
-	//start at PC = 0;
-	static unsigned pc = 0;
-	return prog[pc++];
+typedef struct haxle_instance {
+	bool running; //still executing if set
+
+	int* bytecode; //bytecode to execute in this instance
+	unsigned pc; //program counter
+
+	cpu_state state; //register state
+} haxle_instance;
+
+int fetch(haxle_instance* vm) {
+	return vm->bytecode[vm->pc++];
 }
 
 instruction* decode(int instr) { 
@@ -24,8 +31,9 @@ instruction* decode(int instr) {
 	return ret;
 }
 
-static bool running = true;
-void eval(cpu_state* state, instruction* instr) {
+void eval(haxle_instance* vm, instruction* instr) {
+	cpu_state* state = &vm->state;
+
 	switch (instr->opcode) {
 		case MOV_INSTR:
 			printf("MOV R%d, #%d", instr->regs[0], instr->immediate);
@@ -40,29 +48,35 @@ void eval(cpu_state* state, instruction* instr) {
 			//fall through to HLT case
 		case HLT_INSTR:
 			printf("HLT");
-			running = false;
+			vm->running = false;
 			break;
 	}
 	printf("\n");
 }
 
-void dump_cpu_state(cpu_state* state) {
+void dump_cpu_state(cpu_state state) {
 	for (int i = 0; i < NUM_REGS; i++) { 
-		printf("R%d = %d\t", i, state->regs[i]);
+		printf("R%d = %d\t", i, state.regs[i]);
 	}
 	printf("\n");
 }
 
-void run(int* prog) {
-	cpu_state* state = malloc(sizeof(cpu_state));
-	memset(state, 0, sizeof(cpu_state));
-
-	while (running) {
-		unsigned instr = fetch(prog);
+void run(haxle_instance* vm) {
+	while (vm->running) {
+		unsigned instr = fetch(vm);
 		instruction* real = decode(instr);
-		eval(state, real);
-		dump_cpu_state(state);
+		eval(vm, real);
+		dump_cpu_state(vm->state);
 	}
+}
+
+haxle_instance* haxle_init(int* bytecode) {
+	haxle_instance* vm = malloc(sizeof(haxle_instance));
+	memset(vm, 0, sizeof(haxle_instance));
+	vm->running = true;
+	vm->bytecode = bytecode;
+	vm->pc = 0;
+	return vm;
 }
 
 int main(int argc, char** argv) {
@@ -71,7 +85,6 @@ int main(int argc, char** argv) {
 		return 1;
 	}
 
-	//int prog[] = {0x1064, 0x11C8, 0x2201, 0x0000};
 	char* name = argv[1];
 	FILE* fp = fopen(name, "r");
 	if (!fp) {
@@ -79,10 +92,12 @@ int main(int argc, char** argv) {
 		return 1;
 	}
 
-	int prog[2048];
-	fread(&prog, sizeof(int), sizeof(prog), fp);
+#define MAX_SIZE 2048
+	int* prog = malloc(sizeof(int) * MAX_SIZE);
+	fread(prog, sizeof(int), MAX_SIZE, fp);
+	haxle_instance* vm = haxle_init(prog);
 
-	run(prog);
+	run(vm);
 
 	return 0;
 }
